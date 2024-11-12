3906e68c067805e422d18e2203e475d13eab08bc
Shachar Sharon <synarete@xxxxxxxx>
2024-11-12 15:42:50 +0200

    Reorder init files
    Signed-off-by: Shachar Sharon <synarete@xxxxxxxx>

--- init.el
+++ init.el
@@ -1,5 +1,9 @@
-;;; init.el --- Initialization file for Emacs -*- lexical-binding: t; -*-
-;; Load minimal configurations:
+;;; init1.el --- Initialization file for Emacs -*- lexical-binding: t; -*-
+;; Load full configurations:
 (load (concat user-emacs-directory "start.el"))
 (load (concat user-emacs-directory "packages.el"))
+(load (concat user-emacs-directory "config.el"))
+(load (concat user-emacs-directory "keybind.el"))
+(load (concat user-emacs-directory "appearance.el"))
+;; (load (concat user-emacs-directory "misc.el"))
 
---
+++ init0.el
@@ -0,0 +1,5 @@
+;;; init.el --- Initialization file for Emacs -*- lexical-binding: t; -*-
+;; Load minimal configurations:
+(load (concat user-emacs-directory "start.el"))
+(load (concat user-emacs-directory "packages.el"))
+
