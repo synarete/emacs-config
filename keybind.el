bebf0fab13f846d8cd60730b98b6b17cf635bc75
Shachar Sharon <synarete@xxxxxxxx>
2024-12-01 09:36:52 +0200

    Use 'C-a' to mark-all-buffer
    Signed-off-by: Shachar Sharon <synarete@xxxxxxxx>

--- keybind.el
+++ keybind.el
@@ -27,7 +27,7 @@
 (global-set-key (kbd "C-x k") 'kill-current-buffer)
 
 ;; Mark all
-(global-set-key (kbd "C-x a") 'mark-whole-buffer)
+(global-set-key (kbd "C-a") 'mark-whole-buffer)
 
 ;; Ctrl-f for search symbol in buffer
 (global-set-key (kbd "C-f") 'isearch-forward-symbol-at-point)
