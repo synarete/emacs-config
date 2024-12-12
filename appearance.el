3626645aa7625557f09aea90ead2ab08df66ef29
Shachar Sharon <synarete@xxxxxxxx>
2024-12-12 10:21:19 +0200

    Set default font to 'Source Code Pro 13'
    Signed-off-by: Shachar Sharon <synarete@xxxxxxxx>

--- appearance.el
+++ appearance.el
@@ -88,6 +88,6 @@
 (setq window-divider-default-right-width 10)
 (window-divider-mode)
 
-;;;; Font
-(add-to-list 'default-frame-alist '(font . "Monospace 12"))
-;; (add-to-list 'default-frame-alist '(font . "Source Code Pro 12"))
+;;;; Default font
+;; (add-to-list 'default-frame-alist '(font . "Monospace 12"))
+(add-to-list 'default-frame-alist '(font . "Source Code Pro 13"))
