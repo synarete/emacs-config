130b1cdc40a2d56d5847fdb6dc618ccb0383d3f0
Shachar Sharon <synarete@xxxxxxxx>
2025-05-22 10:07:42 +0300

    Restore normal GC after startup
    Signed-off-by: Shachar Sharon <synarete@xxxxxxxx>

--- early-init.el
+++ early-init.el
@@ -1,7 +1,12 @@
 ;;; early-init.el --- Start-up settings -*- lexical-binding: t; -*-
 
-;; Increase default garbage collection threshold
-(setq gc-cons-threshold (* 32 1024 1024))
+;; Increase default garbage collection thresholds for startup
+(setq gc-cons-threshold (* 64 1024 1024))
+(setq gc-cons-percentage 0.6)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 8 1024 1024))
+            (setq gc-cons-percentage 0.1)))
 
 ;; Prevent the glimpse of un-styled Emacs by setting these early.
 (add-to-list 'default-frame-alist '(tool-bar-lines . 0))
@@ -10,4 +15,3 @@
 
 ;; One less file to load at startup
 (setq site-run-file nil)
-
