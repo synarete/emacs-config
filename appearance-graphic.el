90e9b8e71f290a41d28847877529213c57f82370
Shachar Sharon <synarete@xxxxxxxx>
2025-08-31 10:20:25 +0300

    Make term-mode a bit darker
    Signed-off-by: Shachar Sharon <synarete@xxxxxxxx>

--- appearance-graphic.el
+++ appearance-graphic.el
@@ -46,10 +46,19 @@
 (setq blink-cursor-interval .7)
 (setq blink-cursor-blinks 7)
 
+;;;; Terminal
 ;; Extend margins in term-mode
 (defun my-term-mode-margins ()
   (setq left-margin-width 2)
   (setq right-margin-width 2))
 
-(add-hook 'term-mode-hook 'my-term-mode-margins)
+;; Make terminal background a bit darker
+(defun my-term-mode-face-remap ()
+  (face-remap-add-relative 'default '(:background "#121212")))
 
+(defun my-term-mode ()
+  (interactive)
+  (my-term-mode-margins)
+  (my-term-mode-face-remap))
+
+(add-hook 'term-mode-hook #'my-term-mode)
