62a8ea2c0d076f2eae92c1dde4addc67558ce537
Shachar Sharon <synarete@xxxxxxxx>
2026-01-04 20:23:11 +0200

    Config ibuffer
    Signed-off-by: Shachar Sharon <synarete@xxxxxxxx>

--- config.el
+++ config.el
@@ -214,65 +214,12 @@
 (add-hook 'c-mode-hook #'my-lsp-c-mode)
 (add-hook 'c-mode-hook #'my-lsp-ui-c-mode)
 
-;;;; IBuffer settings & layout
-(require 'ibuffer)
-(setq ibuffer-saved-filter-groups
-      (quote
-       (("default"
-         ("Debug"
-          (or
-           (mode . gud-mode)
-           (mode . gud-tooltip-mode)
-           (mode . gdb-breakpoints-mode)
-           (mode . gdb-disassembly-mode)
-           (mode . gdb-frames-mode)
-           (mode . gdb-inferior-io-mode)
-           (mode . gdb-locals-mode)
-           (mode . gdb-memory-mode)
-           (mode . gdb-registers-mode)
-           (mode . gdb-script-mode)
-           (mode . gdb-threads-mode)
-           ))
-         ("Shell"
-          (or
-           (mode . term-mode)
-           (mode . shell-mode)
-           (mode . eshell-mode)))
-         ("C/C++"
-          (or
-           (mode . c-mode)
-           (mode . c++-mode)))
-         ("Python"
-          (mode . python-mode))
-         ("List/Scheme"
-          (or
-           (mode . scheme-mode)
-           (mode . lisp-mode)
-           (mode . emacs-lisp-mode)))
-         ("Shell"
-          (or
-           (mode . sh-mode)
-           (mode . shell-script-mode)))
-         ("Text"
-          (or
-           (mode . text-mode)
-           (mode . markdown-mode)
-           (mode . rst-mode)))
-         ))))
-
-(defun my-ibuffer-mode ()
-  (ibuffer-switch-to-saved-filter-groups "default")
-  (setq ibuffer-show-empty-filter-groups nil)
-  (setq ibuffer-default-shrink-to-minimum-size nil)
-  (setq ibuffer-use-other-window nil)
-  (setq ibuffer-display-summary nil)
-  (ibuffer-auto-mode 1))
-
-(add-hook 'ibuffer-mode-hook #'my-ibuffer-mode)
-
 ;;;; Debugging
 (require 'gud)
 (setq gdb-show-main t)
 (setq gdb-restore-window-configuration-after-quit t)
 (setq gdb-display-io-nopopup t)
 (setq gdb-many-windows nil)
+
+;; IBuffer
+(load (concat user-emacs-directory "config-ibuffer.el"))
