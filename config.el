c6cb68babe936318e7487239c8b6afba2d33f10b
Shachar Sharon <synarete@xxxxxxxx>
2026-01-11 09:55:33 +0200

    Config programming
    Signed-off-by: Shachar Sharon <synarete@xxxxxxxx>

--- config.el
+++ config.el
@@ -65,76 +65,21 @@
 ;; Isearch settings
 (setq isearch-lazy-count t)
 
+;; Prevent extraneous Tabs
+(setq-default indent-tabs-mode nil)
+
 ;; Use ido
 (require 'ido)
 (ido-mode t)
 
-;;;; Programming configs
-
-;; Prevent extraneous Tabs
-(setq-default indent-tabs-mode nil)
-
-;; Display line numbers
-(add-hook 'prog-mode-hook #'display-line-numbers-mode)
-
-;; Display column numbers
-(add-hook 'prog-mode-hook #'column-number-mode)
-
-;; Remove trailing whitespaces upon save
-(defun my-prog-mode-save ()
-  (add-hook 'before-save-hook 'delete-trailing-whitespace)
-  (add-hook 'before-save-hook 'whitespace-cleanup))
-
-(add-hook 'prog-mode-hook #'my-prog-mode-save)
-
-;; Show column-indicator at 80
-(add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
-
-;; Enable spell-checker in prog mode
-(add-hook 'prog-mode-hook #'flyspell-prog-mode)
-
-;; Prefered coding-style
-(setq c-default-style
-      '((cc-mode . "linux")
-        (python-mode . "python")
-        (awk-mode . "awk")
-        (java-mode . "java")
-        (other . "gnu")))
-
-;; C style
-(defun my-c-mode-style ()
-  (c-set-style "linux")
-  (setq-local c-indent-level 8)
-  (setq-local tab-always-indent t)
-  (setq-local tab-width 8))
-
-(add-to-list 'c-mode-common-hook #'my-c-mode-style)
-
-;; Shell script style (following Linux kernel convention).
-(defun my-sh-mode-style ()
-  (setq-local tab-width 8)
-  (setq-local tab-always-indent t)
-  (setq-local sh-basic-offset 8)
-  (setq-local sh-indent-after-continuation 'always)
-  (setq-local indent-tabs-mode t))
-
-(add-hook 'sh-mode-hook #'my-sh-mode-style)
-
-;;;; Compilation settings
-(setq-default compilation-always-kill t)
-(setq-default compilation-ask-about-save nil)
-(setq-default compilation-scroll-output 'first-error)
-
-;;;; Flymake
-(require 'flymake)
-(add-hook 'c-mode-hook #'flymake-mode)
-
-
-;; Coding and text-mode 
-(load (concat user-emacs-directory "config-text.el"))
-
 ;; IBuffer
 (load (concat user-emacs-directory "config-ibuffer.el"))
+
+;; Coding and text-mode
+(load (concat user-emacs-directory "config-text.el"))
+
+;; Prgoramming
+(load (concat user-emacs-directory "config-prog.el"))
 
 ;; Debugging
 (load (concat user-emacs-directory "config-gdb.el"))
@@ -142,5 +87,5 @@
 ;; LSP
 (load (concat user-emacs-directory "config-lsp.el"))
 
-;; Command-line interpreters 
+;; Command-line interpreters
 (load (concat user-emacs-directory "config-comint.el"))
