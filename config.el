1ec3a1a3b328321e4cc175f63b5922ed49394e7b
Shachar Sharon <synarete@xxxxxxxx>
2026-01-14 13:12:08 +0200

    A bit of code cleanups
    Signed-off-by: Shachar Sharon <synarete@xxxxxxxx>

--- config.el
+++ config.el
@@ -1,20 +1,26 @@
 ;;; config.el -- User specific configuration -*- lexical-binding: t; -*-
 
-(my-user-config "config-common.el"
-                :needs '(cl-lib))
-(my-user-config "config-comint.el"
-                :needs '(comint))
+(my-user-config "config-common.el")
+
+(my-user-config "config-comint.el")
+
 (my-user-config "config-ibuffer.el"
                 :needs '(ibuffer))
+
 (my-user-config "config-text.el"
                 :needs '(flyspell))
+
 (my-user-config "config-prog.el"
                 :needs '(cc-mode make-mode xref flymake flyspell))
+
 (my-user-config "config-lsp.el"
                 :needs '(lsp-mode lsp-ui which-key))
+
 (my-user-config "config-gdb.el"
                 :needs '(gud))
+
 (my-user-config "config-treemacs.el"
                 :needs '(treemacs))
+
 (my-user-config "config-misc.el"
                 :needs '(ido))
--- helpers.el
+++ helpers.el
@@ -5,13 +5,14 @@
 (cl-defun my-user-config (file-name &key needs)
   "Load a configuration FILE-NAME from the user-emacs-directory."
   (let ((file-path (expand-file-name file-name user-emacs-directory)))
-    (when
-        (or
-         (null needs)
-         (cl-every #'package-installed-p needs))
-      (if (file-exists-p file-path)
-          (load file-path)
-        (message "Config file not found: %s" file-path)))))
+    (cond
+     ((and needs (not (cl-every #'package-installed-p needs)))
+      (message "Not loading %s due to missing packages %s" file-path needs))
+
+     ((not (file-exists-p file-path))
+      (message "File not found: %s" file-path))
+
+     (t (load file-path)))))
 
 (cl-defun my-user-config-if (pred file-name)
   "Load FILE-NAME only if PRED is true."
