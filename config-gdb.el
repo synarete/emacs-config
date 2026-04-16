d4c36dc164ea17ffc2f4076e59e55cbfa3774ab4
Shachar Sharon <synarete@xxxxxxxx>
2026-04-16 15:50:21 +0300

    Back to simple GDB/GUD config
    Signed-off-by: Shachar Sharon <synarete@xxxxxxxx>

--- config-gdb.el
+++ config-gdb.el
@@ -1,8 +1,9 @@
 ;;; config-gdb.el -- Debug (GDB) configuration -*- lexical-binding: t; -*-
 
+;;;; GUD configuration
 (require 'gud)
 (setq gdb-show-main t)
-(setq gdb-restore-window-configuration-after-quit t)
 (setq gdb-display-io-nopopup t)
-(setq gdb-many-windows nil)
 (setq gdb-non-stop-setting nil)
+(setq gdb-restore-window-configuration-after-quit t)
+(setq gdb-many-windows t)
