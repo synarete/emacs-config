794f1df3f5de0c05a080d1e7c0c71c65eeebbc97
Shachar Sharon <synarete@xxxxxxxx>
2026-04-17 11:04:33 +0300

    No auto-open treemacs after-init
    Signed-off-by: Shachar Sharon <synarete@xxxxxxxx>

--- after-init.el
+++ after-init.el
@@ -1,7 +1,7 @@
 ;;; after-init.el  -*- lexical-binding: t; -*-
 
 ;; Open treemacs
-(add-hook 'after-init-hook #'treemacs-select-window)
+;; (add-hook 'after-init-hook #'treemacs-select-window)
 
 ;; Full-screen by default
 (add-hook 'after-init-hook #'toggle-frame-fullscreen)
