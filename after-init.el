4acecd5026387145355eedc438ec189510cba733
Shachar Sharon <synarete@xxxxxxxx>
2026-05-27 10:03:05 +0300

    Fullscreen in GUI mode only
    Signed-off-by: Shachar Sharon <synarete@xxxxxxxx>

--- after-init.el
+++ after-init.el
@@ -4,9 +4,12 @@
 ;; (add-hook 'after-init-hook #'treemacs-select-window)
 
 ;; Full-screen by default
-(add-hook 'after-init-hook #'toggle-frame-fullscreen)
+(defun my-frame-size ()
+  (when (display-graphic-p)
+    (toggle-frame-fullscreen)))
+
+(add-hook 'after-init-hook #'my-frame-size)
 
 ;; Load custom file
 (when (file-exists-p custom-file)
   (load custom-file 'noerror))
-
